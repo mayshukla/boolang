40c28aa53a8587c3b4d35d943a5dfe8e68fd0478
ryan <4721319+rpshukla@users.noreply.github.com>
2018-11-15 15:08:13 -0700

    Adds Environment.py

---
+++ src/Environment.py
@@ -0,0 +1,29 @@
+class Environment:
+    """ A class to store bindings of identifiers to values.
+        Basically, a namespace.
+    """
+
+    # Dictionary where the keys are identifiers as plain strings,
+    # values are the values associated with that identifier
+    mappings = {}
+
+    def __init__(self):
+        # Start with no mappings
+        self.mappings = {}
+
+    def bind(self, identifier, value):
+        """ Binds a new identifier or re-defines an old one
+        """
+        self.mappings[identifier] = value
+
+
+    def lookup(self, identifier):
+        """ Returns the value associated with identifier
+            If identifier has not been bound yet, throws an error
+        """
+        try:
+            return self.mappings[identifier]
+        except KeyError:
+            raise Exception('boolang error: variable "{}" not defined'
+                            .format(identifier))
+
